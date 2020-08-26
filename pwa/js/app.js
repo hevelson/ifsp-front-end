d867c69a6f3661d5da18c285ed622906380f7c58
Hevelson <hevelson@gmail.com>
2020-08-25 21:28:28 -0300

    criado contador de vida para magic the gathering

--- pwa/js/app.js
+++ pwa/js/app.js
@@ -4,51 +4,30 @@ if ("serviceWorker" in navigator) {
       .register("/sw.js")
       .then(res => console.log("service worker registered"))
       .catch(err => console.log("service worker not registered", err))
-  })
+  });
 }
 
-if (navigator.mediaDevices && typeof navigator.mediaDevices.getUserMedia === 'function') {
-
-  document.addEventListener('DOMContentLoaded', function () {
-    let options = {
-      onCloseStart: fecharCamera,
-      onOpenStart: iniciarCamera
-    }
-
-    let elems = document.querySelectorAll('.modal');
-    let instances = M.Modal.init(elems, options);
-  });
-
-  function fecharCamera() {
-    Quagga.stop();
+const changePlayerLife = (player, operation, amount) => {
+  let playerLifeInput = document.getElementById(`player-${player}-life`);
+  const life = parseInt(playerLifeInput.value);
+  switch (operation) {
+    case "plus":
+      playerLifeInput.value = life + amount;
+      break;
+    case "minus":
+      playerLifeInput.value = life - amount;
+      break;
+    default:
+      playerLifeInput.value = 20;
+      break;
   }
+}
 
-  function iniciarCamera() {
-    Quagga.init({
-      inputStream: {
-        name: "Live",
-        type: "LiveStream",
-        target: document.querySelector('#barcode-scanner')
-      },
-      decoder: {
-        readers: ["code_128_reader"]
-      }
-    }, function (err) {
-      if (err) {
-        console.log(err);
-        return
-      }
-      console.log("Initialization finished. Ready to start");
-      Quagga.start();
-    });
-  }
+const btnPlusLife = document.querySelectorAll('.update-life');
 
-  Quagga.onDetected(function (result) {
-    const code = result.codeResult.code;
-    console.log(code);
-    document.querySelector("#codigo").value = code;
-    const modal = M.Modal.getInstance(document.querySelector("#modal1"));
-    modal.close();
+for (const button of btnPlusLife) {
+  button.addEventListener('click', function(event) {
+    const { player, operation } = button.dataset;
+    changePlayerLife(player, operation, 1);
   });
-  // safely access `navigator.mediaDevices.getUserMedia`
 }
